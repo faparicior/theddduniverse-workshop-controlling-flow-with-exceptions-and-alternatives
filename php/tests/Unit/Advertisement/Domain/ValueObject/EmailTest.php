bd74ffb968be91625a28d631bc4b8fc42bb7de36
Fernando Aparicio <faparicior@gmail.com>
2024-05-18 16:49:10 +0200

    Add tests

---
+++ php/tests/Unit/Advertisement/Domain/ValueObject/EmailTest.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Demo\App\Unit\Advertisement\Domain\ValueObject;
+
+use Demo\App\Advertisement\Domain\Model\ValueObject\Email;
+use PHPUnit\Framework\TestCase;
+
+class EmailTest extends TestCase
+{
+    private const string EMAIL = 'email@test.com';
+    private const string INVALID_EMAIL = 'emailtest.com';
+
+    public function testShouldCreateAnEmail()
+    {
+        $email = new Email(self::EMAIL);
+        $this->assertEquals(self::EMAIL, $email->value());
+    }
+
+    public function testShouldThrowAnExceptionWhenEmailIsInvalid()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        new Email(self::INVALID_EMAIL);
+    }
+}
