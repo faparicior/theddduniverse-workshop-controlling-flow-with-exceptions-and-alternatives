f7ef051b43a8ce7c5a1f4f9a193dcf42b99333f6
Fernando Aparicio <faparicior@gmail.com>
2024-05-26 17:59:08 +0200

    Avoid stack trace

--- kotlin/src/main/kotlin/advertisement/domain/exceptions/DescriptionEmptyException.kt
+++ kotlin/src/main/kotlin/advertisement/domain/exceptions/DescriptionEmptyException.kt
@@ -7,4 +7,16 @@ class DescriptionEmptyException private constructor(message: String) : Exception
             return DescriptionEmptyException("Description empty")
         }
     }
+
+    override fun fillInStackTrace(): Throwable {
+        if (shouldIncludeStackTrace()) {
+            return super.fillInStackTrace()
+        }
+        return this
+    }
+
+    private fun shouldIncludeStackTrace(): Boolean {
+        // Implement logic to determine if stack trace is needed (e.g., based on configuration)
+        return false
+    }
 }
