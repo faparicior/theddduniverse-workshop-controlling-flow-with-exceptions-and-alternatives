7c6c82d658c8fe77bff64fe2aa42fef6a4f3663c
Fernando Aparicio <faparicior@gmail.com>
2024-06-04 19:20:21 +0200

    Add domain exceptions

--- typescript/src/advertisement/ui/Http/PublishAdvertisementController.ts
+++ typescript/src/advertisement/ui/Http/PublishAdvertisementController.ts
@@ -3,6 +3,7 @@ import { FrameworkResponse } from '../../../framework/FrameworkResponse';
 import { PublishAdvertisementCommand } from '../../application/publish-advertisement/PublishAdvertisementCommand';
 import { PublishAdvertisementUseCase } from '../../application/publish-advertisement/PublishAdvertisementUseCase';
 import {CommonController} from "../../../common/ui/CommonController";
+import {BoundedContextException} from "../../../common/exceptions/BoundedContextException";
 
 type AddAdvertisementRequest = FrameworkRequest & {
   body: {
@@ -33,7 +34,12 @@ export class PublishAdvertisementController extends CommonController {
 
       return this.processSuccessfulCreateCommand()
     } catch (error: any) {
-      return this.processFailedCommand(error)
+      switch (true) {
+        case error instanceof BoundedContextException:
+          return this.processDomainOrApplicationExceptionResponse(error)
+        default:
+          return this.processFailedCommand(error)
+      }
     }
   }
 }
